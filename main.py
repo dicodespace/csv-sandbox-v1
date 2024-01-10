35e85f9478921a24b00c2a1d38d73c3605df1817
disha gandhi <dicodespace@gmail.com>
2024-01-10 20:23:49 +0530

    dict reader

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv 
 
 with open("records.csv","r") as csv_file:
-    csv_reader = csv.reader(csv_file)
-
+#  csv_reader = csv.reader(csv_file)
+    csv_reader=csv.DictReader(csv_file)
+    
     for row in csv_reader:
         print(row)
