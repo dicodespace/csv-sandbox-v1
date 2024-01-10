6df44aea1228818857fb9b1874b277354bc350dd
disha gandhi <dicodespace@gmail.com>
2024-01-10 20:53:46 +0530

    add function based parsing for csv

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import csv 
 
-with open("records.csv","r") as csv_file:
-#  csv_reader = csv.reader(csv_file)
-    csv_reader=csv.DictReader(csv_file)
-    
-    for row in csv_reader:
-        print(row)
+def main(file_path):
+    with open(file_path,"r") as csv_file:
+        csv_reader=csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            print(row)
+
+main("records.csv")
+main("users.csv")
