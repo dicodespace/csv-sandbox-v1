9d82f3b4b012f41d524c6052c47bb4bc3b198b41
disha gandhi <dicodespace@gmail.com>
2024-01-24 22:04:07 +0530

    printed against each row

--- sample.py
+++ sample.py
@@ -4,15 +4,20 @@ def main(filepath):
     total_revenue=0
     transaction_count=0
     with open(filepath,'r') as csv_file:
-        spam_reader=csv.reader(csv_file)
+        spam_reader=csv.DictReader(csv_file)
         header=next(spam_reader)
 
         for row in spam_reader:            
-            print(row[1])
-        total_revenue += float(row[3]) + float(row[2])
-        avg_transaction = total_revenue / float(row[4])
+            center_name=row['center_name']
+            total_received = float(row['total_received'])
+            total_pending = float(row['total_pending'])
+            total_transactions = float(row['total_transactions'])
 
-    print(f"Total Revenue: {total_revenue}")
-    print(f"Avg Transaction across all centers: {avg_transaction}")   
+            total_revenue += total_pending + total_received
+            avg_transaction = total_revenue / total_transactions
+
+            print(f"Center: {center_name}")
+            print(f"Total Revenue: {total_revenue}")
+            print(f"Avg Transaction : {avg_transaction}")   
 
 main("sample_center_data_v1.csv")
